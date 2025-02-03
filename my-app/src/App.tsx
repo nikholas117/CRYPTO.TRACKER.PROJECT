91c795493ce38d955211ee4ed816539f28757860
nikholas117 <nikholas123@hotmail.com>
2025-02-02 23:23:18 -0500

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import CryptoTracking from "./CryptoTracking";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <CryptoTracking />
+    </div>
+  );
+};
+
+export default App;
